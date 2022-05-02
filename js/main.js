737083b60953ce3b6215b1afe810ac8176b01cba
salamone-gius <salamone.gius@gmail.com>
2022-05-02 20:52:25 +0200

    style added

--- js/main.js
+++ js/main.js
@@ -6,14 +6,12 @@
 // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 // MILESTONE 3
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
                     // --- SOLUTION --- \\
 
+let container = document.querySelector("div.container");
+let box = document.createElement("div");
+                
 //stampo in console i numeri da 1 a 100
 for (i = 1; i <= 100; i++) {
     //dichiaro le variabili per i multipli e gli assegno un valore
@@ -22,18 +20,52 @@ for (i = 1; i <= 100; i++) {
     //imposto le condizioni per sostituire i numeri interessati
     //SE fizz E buzz sono entrambe VERE (numero multiplo sia di 3 che di 5)
     if ((fizz) && (buzz)) {
-        //ALLORA stampo in console "FizzBuzz" al posto del numero
+        //ALLORA stampo in console "FizzBuzz" al posto del numero;
         console.log("FizzBuzz");
+        //creo un div,
+        let box = document.createElement("div");
+        //gli aggiungo le classi CSS,
+        box.classList.add("box", "pink");
+        //lo attacco al container,
+        container.append(box);
+        //stampo all'interno;
+        box.innerHTML = "FizzBuzz";
         //ALTRIMENTI, SE fizz è VERO (numero multiplo di 3)
     } else if (fizz) {
         //ALLORA stampo in console "Fizz" al posto del numero
         console.log("Fizz");
+        //creo un div,
+        let box = document.createElement("div");
+        //gli aggiungo le classi CSS,
+        box.classList.add("box", "green");
+        //lo attacco al container,
+        container.append(box);
+        //stampo all'interno;
+        box.innerHTML = "Fizz";
         //ALTRIMENTI, SE buzz è VERO (numero multiplo di 5)
     } else if (buzz) {
-        //ALLORA stampo in console "Fizz" al posto del numero
+        //ALLORA stampo in console "Buzz" al posto del numero
         console.log("Buzz");
+        //creo un div,
+        let box = document.createElement("div");
+        //gli aggiungo le classi CSS,
+        box.classList.add("box", "yellow");
+        //lo attacco al container,
+        container.append(box);
+        //stampo all'interno;
+        box.innerHTML = "Buzz";
+        //ALTRIMENTI
     } else {
+        //stampo il numero in console
         console.log(i);
+        //creo un div,
+        let box = document.createElement("div");
+        //gli aggiungo le classi CSS,
+        box.classList.add("box", "blue");
+        //lo attacco al container,
+        container.append(box);
+        //stampo all'interno;
+        box.innerHTML = i;
     }
 }
 
